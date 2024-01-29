618dcba90bcfa5d8c50704ab3da02a1417a1a738
Mosharof-Hossain <mosharof.it.uits@gmail.com>
2024-01-29 20:47:47 +0600

    Hukum Shop

---
+++ android/app/src/main/kotlin/com/example/hukum_online_shop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hukum_online_shop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'hukum.dart';
+
+void main() {
+  runApp(const Hukum());
+}
+
+
+
